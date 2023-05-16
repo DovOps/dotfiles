ede8b6a90297d1de4f4de37b398f18d95aa289c0
DovOps <dov.katz@morganstanley.com>
2023-05-16 16:40:04 +0000

    removed nvm added github cli

--- host-setups/ubuntu_20.04.sh
+++ host-setups/ubuntu_20.04.sh
@@ -8,11 +8,20 @@ rm packages-microsoft-prod.deb
 sudo apt-get update && \
   sudo apt-get install -y dotnet-sdk-7.0
 
+# update package list for github cli
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+# github cli login
+sudo apt update
+sudo apt-get  install -y gh
 
 sudo apt-get  install -y gradle curl git  openjdk-17-jre-headless 
-sudo apt-get  -y remove nodejs
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-source ~/.bashrc
+
 
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs

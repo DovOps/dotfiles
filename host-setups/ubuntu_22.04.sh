81b76740fdee4a9b4a1e9a7d248ebe518121aa53
DovOps <dov@dovkatz.net>
2023-06-20 11:46:23 +0000

    Update ubuntu_22.04.sh
    added docker, docker compose and the right node

--- host-setups/ubuntu_22.04.sh
+++ host-setups/ubuntu_22.04.sh
@@ -39,6 +39,21 @@ sudo apt install snapd
 # install code-insiders
 sudo snap install code-insiders --classic
 
+# DOCKER 
+# Credits: https://linuxhint.com/install-docker-compose-ubuntu-22-04/
+sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt install -y docker-ce
+
+# Docker Compose
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+
 # option to run code-insiders
 code-insiders tunnel --accept-server-license-terms
 

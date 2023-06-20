d7fbac2d73319b419ca27ab3c88be58edfef45a2
DovOps <dov@dovkatz.net>
2023-06-20 11:35:37 +0000

    Create ubuntu_22.04.sh

---
+++ host-setups/ubuntu_22.04.sh
@@ -0,0 +1,45 @@
+
+sudo apt update
+#get .net package list
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-7.0
+
+# update package list for github cli
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+# github cli login
+sudo apt update
+sudo apt-get  install -y gh
+
+sudo apt-get  install -y gradle curl git  openjdk-17-jre-headless 
+
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.profile 
+nvm install node 
+
+#install code-server
+wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
+
+#install code-insiders code cli tunnel
+
+# for pre 22.x ubuntu
+sudo apt update
+sudo apt install snapd
+
+# install code-insiders
+sudo snap install code-insiders --classic
+
+# option to run code-insiders
+code-insiders tunnel --accept-server-license-terms
+
+
